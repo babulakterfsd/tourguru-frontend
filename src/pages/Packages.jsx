f8de9956dd120054e509f061ac0dcf6bbad0bd09
Babul Akter <babulakterfsd@gmail.com>
2022-07-24 19:19:00 +0600

    skeleton added

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -13,56 +13,6 @@ function Packages() {
     const { mobile, tablet, desktop, allPackages, orderData, activeStep, setActiveStep } =
         useAuth();
 
-    if (allPackages?.length === 0) {
-        return (
-            <Container>
-                <Box
-                    style={{
-                        height: `100vh`,
-                        display: `flex`,
-                        justifyContent: `center`,
-                        alignItems: `center`,
-                        margin: mobile ? `150px 0px` : `180px 15px`,
-                    }}
-                >
-                    {mobile ? (
-                        <Grid container spacing={3}>
-                            {Array.from(Array(3)).map((packages, index) => (
-                                <Grid item xs={12}>
-                                    <Skeleton
-                                        variant="rectangular"
-                                        style={{
-                                            width: `100%`,
-                                            height: mobile ? `150px` : `210px`,
-                                        }}
-                                    />
-                                    <Skeleton />
-                                    <Skeleton width="60%" />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    ) : (
-                        <Grid container spacing={3}>
-                            {Array.from(Array(9)).map((packages, index) => (
-                                <Grid item md={6} lg={4}>
-                                    <Skeleton
-                                        variant="rectangular"
-                                        style={{
-                                            width: `100%`,
-                                            height: mobile ? `150px` : `210px`,
-                                        }}
-                                    />
-                                    <Skeleton />
-                                    <Skeleton width="60%" />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    )}
-                </Box>
-            </Container>
-        );
-    }
-
     return (
         <div
             style={{
@@ -94,19 +44,70 @@ function Packages() {
                     quaerat! Culpa, inventore. Doloribus praesentium dicta repellendus tempora
                     eligendi maxime odio soluta quae in, architecto modi maiores quas facere?
                 </Typography>
-                <Box style={{ padding: mobile ? `40px 0px` : `80px 0px` }}>
-                    <Grid
-                        container
-                        spacing={{ xs: 2, md: 3, lg: 5 }}
-                        columns={{ xs: 12, md: 12, lg: 12 }}
-                    >
-                        {allPackages?.map((singlePackage, index) => (
-                            <Grid item xs={12} md={6} lg={4} key={index}>
-                                <SinglePackageCard singlePackage={singlePackage} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
+                {allPackages?.length === 0 ? (
+                    <Container>
+                        <Box
+                            style={{
+                                height: `100vh`,
+                                display: `flex`,
+                                justifyContent: `center`,
+                                alignItems: `center`,
+                                margin: mobile ? `100px 0px` : `180px 15px`,
+                            }}
+                        >
+                            {mobile ? (
+                                <>
+                                    <ScrollToTop />
+                                    <Grid container spacing={3}>
+                                        {Array.from(Array(3)).map((packages, index) => (
+                                            <Grid item xs={12}>
+                                                <Skeleton
+                                                    variant="rectangular"
+                                                    style={{
+                                                        width: `100%`,
+                                                        height: mobile ? `150px` : `210px`,
+                                                    }}
+                                                />
+                                                <Skeleton />
+                                                <Skeleton width="60%" />
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </>
+                            ) : (
+                                <Grid container spacing={5}>
+                                    {Array.from(Array(9)).map((packages, index) => (
+                                        <Grid item md={6} lg={4}>
+                                            <Skeleton
+                                                variant="rectangular"
+                                                style={{
+                                                    width: `100%`,
+                                                    height: mobile ? `150px` : `210px`,
+                                                }}
+                                            />
+                                            <Skeleton />
+                                            <Skeleton width="60%" />
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            )}
+                        </Box>
+                    </Container>
+                ) : (
+                    <Box style={{ padding: mobile ? `40px 0px` : `80px 0px` }}>
+                        <Grid
+                            container
+                            spacing={{ xs: 2, md: 3, lg: 5 }}
+                            columns={{ xs: 12, md: 12, lg: 12 }}
+                        >
+                            {allPackages?.map((singlePackage, index) => (
+                                <Grid item xs={12} md={6} lg={4} key={index}>
+                                    <SinglePackageCard singlePackage={singlePackage} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                )}
             </Container>
         </div>
     );
