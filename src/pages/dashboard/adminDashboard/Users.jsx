9769bea918f636718d0556ee43763f0761471009
Babul Akter <babulakterfsd@gmail.com>
2022-07-27 16:43:09 +0600

    table customized

--- src/pages/dashboard/adminDashboard/Users.jsx
+++ src/pages/dashboard/adminDashboard/Users.jsx
@@ -1,13 +1,42 @@
+/* eslint-disable no-unused-vars */
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import ScrollToTop from '../../../components/ScrollToTop';
 import useAuth from '../../../hooks/useAuth';
+import UsersTable from './UsersTable';
 
 function Users() {
-    const { allUsers } = useAuth();
-    console.log(allUsers);
+    const { user, allUsers, mobile } = useAuth();
+
     return (
-        <div>
-            <p>this is users list</p>
-        </div>
+        <Container>
+            <ScrollToTop />
+            <Box>
+                <Typography color="primary" style={{ textAlign: `center` }}>
+                    All Users
+                </Typography>
+                <Typography
+                    color="bluishDark"
+                    variant="h4"
+                    style={{
+                        textAlign: `center`,
+                        fontFamily: `abril`,
+                        fontWeight: `700`,
+                        color: '#283A5E',
+                        letterSpacing: `-1px`,
+                    }}
+                >
+                    All Registered Users List
+                </Typography>
+                <Typography style={{ textAlign: `center`, fontFamily: `poppins` }}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam fugit inventore
+                    quaerat! Culpa, inventore.
+                </Typography>
+                <Box style={{ padding: mobile ? `0px` : `50px` }}>
+                    <UsersTable />
+                </Box>
+            </Box>
+        </Container>
     );
 }
 
--- src/pages/dashboard/commonDashboard/Profile.jsx
+++ src/pages/dashboard/commonDashboard/Profile.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 function Profile() {
+    const { user } = useAuth();
+
     return (
         <div>
             <p>This is user profile</p>
