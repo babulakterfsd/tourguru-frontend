6b99b416da1a16a5278411c2477a5c586525791c
Babul Akter <babulakterfsd@gmail.com>
2022-08-25 20:39:12 +0600

    skeleton showing logic improved while fetching order data in the dashboard

--- src/pages/dashboard/adminDashboard/AllOrders.jsx
+++ src/pages/dashboard/adminDashboard/AllOrders.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Skeleton, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ScrollToTop from '../../../components/ScrollToTop';
@@ -9,14 +9,67 @@ import AllOrderTable from './AllOrderTable';
 
 function AllOrders() {
     const { user, allUsers, mobile } = useAuth();
-    const [allOrders, setAllOrders] = useState([]);
+    const [allOrders, setAllOrders] = useState(null);
 
     const getAllOrdersURL = `http://localhost:5000/allorder`;
     useEffect(() => {
         axios.get(getAllOrdersURL).then((result) => setAllOrders(result?.data));
     }, [getAllOrdersURL]);
 
-    if (allOrders?.length === 0) {
+    if (!Array.isArray(allOrders)) {
+        return (
+            <Container>
+                <Box
+                    style={{
+                        height: `100vh`,
+                        display: `flex`,
+                        justifyContent: `center`,
+                        alignItems: `center`,
+                        margin: mobile ? `100px 0px` : `180px 15px`,
+                    }}
+                >
+                    {mobile ? (
+                        <>
+                            <ScrollToTop />
+                            <Grid container spacing={3}>
+                                {Array.from(Array(3)).map((packages, index) => (
+                                    <Grid item xs={12} key={index}>
+                                        <Skeleton
+                                            variant="rectangular"
+                                            style={{
+                                                width: `100%`,
+                                                height: mobile ? `150px` : `210px`,
+                                            }}
+                                        />
+                                        <Skeleton />
+                                        <Skeleton width="60%" />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </>
+                    ) : (
+                        <Grid container spacing={5}>
+                            {Array.from(Array(9)).map((packages, index) => (
+                                <Grid item md={6} lg={4} key={index}>
+                                    <Skeleton
+                                        variant="rectangular"
+                                        style={{
+                                            width: `100%`,
+                                            height: mobile ? `150px` : `210px`,
+                                        }}
+                                    />
+                                    <Skeleton />
+                                    <Skeleton width="60%" />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+                </Box>
+            </Container>
+        );
+    }
+
+    if (Array.isArray(allOrders) && allOrders?.length === 0) {
         return (
             <Container>
                 <Box
@@ -28,9 +81,9 @@ function AllOrders() {
                     }}
                 >
                     <Typography
-                        variant="h5"
+                        variant="h4"
                         style={{ color: '#1a213d' }}
-                        data-aos="flip-left"
+                        data-aos="zoom-in"
                         data-aos-duration="1500"
                     >
                         No Order Found
--- src/pages/dashboard/userDashboard/MyOrders.jsx
+++ src/pages/dashboard/userDashboard/MyOrders.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Skeleton, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ScrollToTop from '../../../components/ScrollToTop';
@@ -9,13 +9,13 @@ import MyOrderTable from './MyOrderTable';
 
 function MyOrders() {
     const { user, allUsers, mobile } = useAuth();
-    const [myOrders, setMyOrders] = useState([]);
+    const [myOrders, setMyOrders] = useState(null);
     const getMyOrdersURL = `http://localhost:5000/myorders/${user?.email}`;
     useEffect(() => {
         axios.get(getMyOrdersURL).then((result) => setMyOrders(result?.data));
     }, [getMyOrdersURL]);
 
-    if (myOrders?.length === 0) {
+    if (!Array.isArray(myOrders)) {
         return (
             <Container>
                 <Box
@@ -27,10 +27,62 @@ function MyOrders() {
                         margin: mobile ? `100px 0px` : `180px 15px`,
                     }}
                 >
+                    {mobile ? (
+                        <>
+                            <ScrollToTop />
+                            <Grid container spacing={3}>
+                                {Array.from(Array(3)).map((packages, index) => (
+                                    <Grid item xs={12} key={index}>
+                                        <Skeleton
+                                            variant="rectangular"
+                                            style={{
+                                                width: `100%`,
+                                                height: mobile ? `150px` : `210px`,
+                                            }}
+                                        />
+                                        <Skeleton />
+                                        <Skeleton width="60%" />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </>
+                    ) : (
+                        <Grid container spacing={5}>
+                            {Array.from(Array(9)).map((packages, index) => (
+                                <Grid item md={6} lg={4} key={index}>
+                                    <Skeleton
+                                        variant="rectangular"
+                                        style={{
+                                            width: `100%`,
+                                            height: mobile ? `150px` : `210px`,
+                                        }}
+                                    />
+                                    <Skeleton />
+                                    <Skeleton width="60%" />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+                </Box>
+            </Container>
+        );
+    }
+
+    if (Array.isArray(myOrders) && myOrders?.length === 0) {
+        return (
+            <Container>
+                <Box
+                    style={{
+                        display: `flex`,
+                        justifyContent: `center`,
+                        alignItems: `center`,
+                        margin: mobile ? `100px 0px` : `180px 15px`,
+                    }}
+                >
                     <Typography
-                        variant="h5"
+                        variant="h4"
                         style={{ color: '#1a213d' }}
-                        data-aos="flip-left"
+                        data-aos="zoom-in"
                         data-aos-duration="1500"
                     >
                         No Order Found
