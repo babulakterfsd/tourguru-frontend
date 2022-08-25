02066927e2556e29d9ccf7f7bef7e98775a8bfad
Babul Akter <babulakterfsd@gmail.com>
2022-08-25 17:02:03 +0600

    skeletons removed from myorder and allorders while loading orders from the backend

--- src/pages/dashboard/adminDashboard/AllOrders.jsx
+++ src/pages/dashboard/adminDashboard/AllOrders.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
-import { Box, Container, Grid, Skeleton } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ScrollToTop from '../../../components/ScrollToTop';
@@ -21,49 +21,20 @@ function AllOrders() {
             <Container>
                 <Box
                     style={{
-                        height: `100vh`,
                         display: `flex`,
                         justifyContent: `center`,
                         alignItems: `center`,
                         margin: mobile ? `100px 0px` : `180px 15px`,
                     }}
                 >
-                    {mobile ? (
-                        <>
-                            <ScrollToTop />
-                            <Grid container spacing={3}>
-                                {Array.from(Array(3)).map((packages, index) => (
-                                    <Grid item xs={12} key={index}>
-                                        <Skeleton
-                                            variant="rectangular"
-                                            style={{
-                                                width: `100%`,
-                                                height: mobile ? `150px` : `210px`,
-                                            }}
-                                        />
-                                        <Skeleton />
-                                        <Skeleton width="60%" />
-                                    </Grid>
-                                ))}
-                            </Grid>
-                        </>
-                    ) : (
-                        <Grid container spacing={5}>
-                            {Array.from(Array(9)).map((packages, index) => (
-                                <Grid item md={6} lg={4} key={index}>
-                                    <Skeleton
-                                        variant="rectangular"
-                                        style={{
-                                            width: `100%`,
-                                            height: mobile ? `150px` : `210px`,
-                                        }}
-                                    />
-                                    <Skeleton />
-                                    <Skeleton width="60%" />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    )}
+                    <Typography
+                        variant="h5"
+                        style={{ color: '#1a213d' }}
+                        data-aos="flip-left"
+                        data-aos-duration="1500"
+                    >
+                        No Order Found
+                    </Typography>
                 </Box>
             </Container>
         );
