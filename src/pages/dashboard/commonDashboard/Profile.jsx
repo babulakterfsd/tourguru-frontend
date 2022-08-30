b3df25049dc13fdab2b8db7abcb41d2dbef3de4c
Babul Akter <babulakterfsd@gmail.com>
2022-08-30 12:53:24 +0600

    image uploading user experience improved by managing states

--- src/pages/dashboard/commonDashboard/Profile.jsx
+++ src/pages/dashboard/commonDashboard/Profile.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable prettier/prettier */
 // import userImage from '../../../assests/images/userdefault.png';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import {
-    Box, Button, Card,
+    Box,
+    Button,
+    Card,
     CardActions,
     CardContent,
     Container,
@@ -15,7 +18,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import { deleteUser } from 'firebase/auth';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import defaultAvatar from '../../../assests/images/userdefault.png';
@@ -24,144 +27,246 @@ import Styles from '../../../styles/Login.module.css';
 import Classes from '../../../styles/Profile.module.css';
 import ProfileDetails from '../adminDashboard/ProfileDetails';
 
-
 function Profile(props) {
-    const {user, userImageURL, setUserImageURL, userInfoInDatabase, setUserInfoInDatabase, setUser, setIsLoading, auth} = useAuth()
+    const [selectedImage, setSelectedImage] = useState(null);
+  const {
+    user,
+    userImageURL,
+    setUserImageURL,
+    userInfoInDatabase,
+    setUserInfoInDatabase,
+    setUser,
+    setIsLoading,
+    auth,
+  } = useAuth();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleUserImageUpload = (e) => {
-        const image = e.target.files[0]
-        const formData = new FormData()
-        formData.set("image", image)
-        const imageStorageAPIKey = `99f27926739ee425304cd99a6447e360`
-        const imgUploadURL = `https://api.imgbb.com/1/upload?key=${imageStorageAPIKey}`
-        axios.post(imgUploadURL, formData)
-            .then((res) => {
-                setUserImageURL(res?.data?.data?.display_url)
-            }).catch((error) => {
-                Swal.fire(`Something went wrong when choosing image`)
-            })
+  const handleUserImageUpload = (e) => {
+    const image = e.target.files[0];
+    if(image) {
+        setSelectedImage('')
     }
+    const formData = new FormData();
+    formData.set('image', image);
+    const imageStorageAPIKey = `99f27926739ee425304cd99a6447e360`;
+    const imgUploadURL = `https://api.imgbb.com/1/upload?key=${imageStorageAPIKey}`;
+    axios
+      .post(imgUploadURL, formData)
+      .then((res) => {
+        setUserImageURL(res?.data?.data?.display_url);
+        setSelectedImage(res?.data?.data?.display_url);
+      })
+      .catch((error) => {
+        Swal.fire(`Something went wrong when choosing image`);
+      });
+  };
 
-    useEffect(() => {
-    }, [userInfoInDatabase,userImageURL]);
+  useEffect(() => {}, [userInfoInDatabase, userImageURL]);
 
-    const saveUserProfilePhoto = (email, photoURL) => {
-        const myUser = { email, img: photoURL};
-        fetch("http://localhost:5000/users", {
-          method: 'PUT',
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(myUser),
-        }).then(res=> res.json())
-           .then((data) => {
-            if(data?.modifiedCount > 0 ) {
-                setUserImageURL('')
-                axios.get(`http://localhost:5000/user/${user?.email}`).then((result) => {
-                    setUserInfoInDatabase(result?.data);
-                });
-                Swal.fire(`Profile photo updated successfully`)
-       }
-           })
-        //    .catch(err => console.log(err.message))
-      };
-
-      const handleDeleteAccount = () => {
-        if(user?.email === 'babulakterfsd@gmail.com') {
-            Swal.fire('You can not delete Head Admin account')
-        } else {
-            deleteUser(user).then(() => {
-                const url = `http://localhost:5000/deleteuser/${user?.email}`;
-            fetch(url, {
-                method: 'DELETE',
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    if (data.deletedCount > 0) {
-                        Swal.fire(`Account deleted successfully`)
-                    }
-                });
-              }).catch((error) => {
-                Swal.fire(`Something went wrong when deleting account`)
-              });
+  const saveUserProfilePhoto = (email, photoURL) => {
+    const myUser = { email, img: photoURL };
+    fetch('http://localhost:5000/users', {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(myUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.modifiedCount > 0) {
+          setUserImageURL('');
+          axios
+            .get(`http://localhost:5000/user/${user?.email}`)
+            .then((result) => {
+              setUserInfoInDatabase(result?.data);
+            });
+          Swal.fire(`Profile photo updated successfully`);
+          setSelectedImage(null);
         }
-      }
+      });
+    //    .catch(err => console.log(err.message))
+  };
 
+  const handleDeleteAccount = () => {
+    if (user?.email === 'babulakterfsd@gmail.com') {
+      Swal.fire('You can not delete Head Admin account');
+    } else {
+      deleteUser(user)
+        .then(() => {
+          const url = `http://localhost:5000/deleteuser/${user?.email}`;
+          fetch(url, {
+            method: 'DELETE',
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.deletedCount > 0) {
+                Swal.fire(`Account deleted successfully`);
+              }
+            });
+        })
+        .catch((error) => {
+          Swal.fire(`Something went wrong when deleting account`);
+        });
+    }
+  };
 
-    return (
-        <Container className={`${Classes.profile}`}>
-            <Grid container spacing={{xs: 2, md: 5}}>
-            <Grid item xs={12} md={4} data-aos="fade-right" data-aos-duration="1500">
-            <Card {...props} style={{position: 'relative'}}>
+  return (
+    <Container className={`${Classes.profile}`}>
+      <Grid container spacing={{ xs: 2, md: 5 }}>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          data-aos="fade-right"
+          data-aos-duration="1500"
+        >
+          <Card {...props} style={{ position: 'relative' }}>
             <CardContent>
-                <Box
-                    sx={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        marginTop: '30px'
+              <Box
+                sx={{
+                  alignItems: 'center',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginTop: '30px',
+                }}
+              >
+                <img
+                  src={
+                    userInfoInDatabase?.img
+                      ? userInfoInDatabase?.img
+                      : defaultAvatar
+                  }
+                  style={{
+                    height: `60px`,
+                    width: `60px`,
+                    borderRadius: `500px`,
+                    objectFit: 'cover',
+                  }}
+                  alt="user"
+                />
+                <Typography color="textPrimary" variant="h5">
+                  {userInfoInDatabase?.displayName}
+                </Typography>
+                {userInfoInDatabase?.city && userInfoInDatabase?.country ? (
+                  <Typography
+                    color="#1A385A"
+                    variant="p"
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      fontSize: '13px',
                     }}
+                  >
+                    <LocationOnIcon style={{ fontSize: '13px' }} />
+                    {`${userInfoInDatabase?.city}, ${userInfoInDatabase?.country}`}
+                  </Typography>
+                ) : null}
+                {userInfoInDatabase?.phone ? (
+                  <Typography
+                    color="#1A385A"
+                    variant="p"
+                    style={{ fontSize: '13px' }}
+                  >
+                    {userInfoInDatabase?.phone}
+                  </Typography>
+                ) : null}
+                <Button
+                  type="button"
+                  variant="text"
+                  style={{
+                    textTransform: 'capitalize',
+                    position: 'absolute',
+                    top: '0px',
+                    right: '0px',
+                  }}
+                  onClick={() => handleDeleteAccount()}
                 >
-                    <img
-                            src={userInfoInDatabase?.img ? userInfoInDatabase?.img : defaultAvatar}
-                            style={{height: `60px`, width: `60px`, borderRadius: `500px`, objectFit: 'cover'}}
-                            alt="user"
-                        />
-                    <Typography color="textPrimary" variant="h5">
-                        {userInfoInDatabase?.displayName}
-                    </Typography>
-                    {
-                        userInfoInDatabase?.city && userInfoInDatabase?.country ? (<Typography color="#1A385A" variant="p" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '13px'}}>
-                        <LocationOnIcon style={{fontSize: '13px'}}/>
-                       {`${userInfoInDatabase?.city}, ${userInfoInDatabase?.country}`}
-                   </Typography>) : null
-                    }
-                    {
-                        userInfoInDatabase?.phone ? (<Typography color="#1A385A" variant="p" style={{fontSize: '13px'}}>
-                        {userInfoInDatabase?.phone}
-                    </Typography>) : null
-                    }
-                    <Button type='button' variant='text' style={{textTransform: 'capitalize', position: 'absolute', top: '0px', right: '0px'}} onClick={() => handleDeleteAccount()}>
-                        Delete Account
-                    </Button>
-                    <Box style={{position: 'absolute', top: '0px', left: '0px', textAlign: 'center', padding: '15px', background: '#f3680b', transform: 'rotate(-45deg) translateY(-15px) translateX(-5px)'}}>
-                    <Typography color="#fff" variant="body2" style={{marginTop: `8px`}}>
-                        {userInfoInDatabase?.role === 'admin' ? `Admin` : `User`}
-                    </Typography>
-                    </Box>
+                  Delete Account
+                </Button>
+                <Box
+                  style={{
+                    position: 'absolute',
+                    top: '0px',
+                    left: '0px',
+                    textAlign: 'center',
+                    padding: '15px',
+                    background: '#f3680b',
+                    transform:
+                      'rotate(-45deg) translateY(-15px) translateX(-5px)',
+                  }}
+                >
+                  <Typography
+                    color="#fff"
+                    variant="body2"
+                    style={{ marginTop: `8px` }}
+                  >
+                    {userInfoInDatabase?.role === 'admin' ? `Admin` : `User`}
+                  </Typography>
                 </Box>
+              </Box>
             </CardContent>
             <Divider />
-            <CardActions style={{display: `flex`, justifyContent: `center`, flexDirection: `column`, alignItems: `center`}}>
-                <TextField
-                            type="file"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="userPhoto"
-                            name="photourl"
-                            className={Styles.customTextField}
-                            style={{border: `0px solid transparent`}}
-                            onChange={(e) => handleUserImageUpload(e)}
-                        />
-                        {
-                            userImageURL !== '' ? (<Button color="primary" variant='contained' fullWidth  style={{textTransform: `none`}} onClick={() => saveUserProfilePhoto(user?.email, userImageURL)}>
-                            Change Photo
-                        </Button>) : (<Button color="primary" variant='contained' disabled fullWidth  style={{textTransform: `none`}} onClick={() => saveUserProfilePhoto(user?.email, userImageURL)}>
-                            Change Photo
-                        </Button>)
-                        }
+            <CardActions
+              style={{
+                display: `flex`,
+                justifyContent: `center`,
+                flexDirection: `column`,
+                alignItems: `center`,
+              }}
+            >
+              <TextField
+                type="file"
+                margin="normal"
+                required
+                fullWidth
+                id="userPhoto"
+                name="photourl"
+                className={Styles.customTextField}
+                style={{ border: `0px solid transparent` }}
+                onChange={(e) => handleUserImageUpload(e)}
+              />
+              {selectedImage === null ? (
+                <Button
+                  color="primary"
+                  variant="contained"
+                  fullWidth
+                  disabled
+                  style={{ textTransform: `none` }}
+                >
+                  Choose Photo
+                </Button>
+              ) : ( selectedImage === '' ? (<Button
+                color="primary"
+                variant="contained"
+                disabled
+                fullWidth
+                style={{ textTransform: `none` }}
+              >
+                Processing Photo
+              </Button>) : (<Button
+                  color="primary"
+                  variant="contained"
+                  fullWidth
+                  style={{ textTransform: `none` }}
+                  onClick={() =>
+                    saveUserProfilePhoto(user?.email, userImageURL)
+                  }
+                >
+                  Upload Photo
+                </Button>)
+              )}
             </CardActions>
-        </Card>
-            </Grid>
-            <Grid item xs={12} md={8} data-aos="fade-left" data-aos-duration="1500">
-                <ProfileDetails/>
-            </Grid>
+          </Card>
         </Grid>
-        </Container>
-    );
+        <Grid item xs={12} md={8} data-aos="fade-left" data-aos-duration="1500">
+          <ProfileDetails />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Profile;
